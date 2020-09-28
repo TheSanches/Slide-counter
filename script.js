b627e1f3f6d69de7e6cd1871edfbb1f8eb1d46d7
Alexandr Zabolotny <saanches7@gmail.com>
2020-09-28 16:40:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function countSlider(slidContainer, slides, flipL, flipR) {
+    let countSlid = 1;
+    const div = document.createElement('div');
+    div.classList.add('count-slides');
+    div.innerHTML = `
+        <span>1</span>
+        <span>/</span>
+        <span></span>
+      `;
+
+      slidContainer.append(div);
+
+    let slidCountWrite = div.querySelectorAll('span');
+
+    slidCountWrite[2].textContent = slides.length;
+
+    flipL.addEventListener('click', () => {
+        if(countSlid > 1){
+            countSlid--;
+        } else {
+            countSlid = slides.length;
+        }
+        slidCountWrite[0].textContent = countSlid;
+    });
+
+    flipR.addEventListener('click', () => {
+        if(countSlid <= slides.length - 1){
+            countSlid++;
+        } else {
+            countSlid = 1;
+        }
+        slidCountWrite[0].textContent = countSlid;
+    });
+
+}
+countSlider(sliderContainer, sliderItem, arrowsItem[0], arrowsItem[1]);
+
+// sliderContainer = parent block, main
+// sliderItem = slides
+// arrowsItem[0], arrowsItem[1] = slide switches
